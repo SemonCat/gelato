883f0f3704b09d66d25329b4f66f29fe9bc7575d
semon_huang <gn00742754@gmail.com>
2015-07-16 09:02:02 +0000

    setup.

--- config/connections.js
+++ config/connections.js
@@ -56,7 +56,7 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  mongodb_dev: {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
